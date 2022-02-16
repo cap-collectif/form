8ab33a7671f78af4d7958370a6a9600f05f74a34
MedElfadhelELHACHEMI <mohamedelfadhel.elhachemi@gmail.com>
2022-02-16 14:24:59 +0100

    init

--- src/components/Form.stories.tsx
+++ src/components/Form.stories.tsx
@@ -1,155 +1,194 @@
-import { Meta } from '@storybook/react';
-import { Box, Flex, FormLabel, FormGuideline } from '@cap-collectif/ui';
-import FormControl from './FormControl';
-import { FieldInput } from './fieldInput';
-import { useForm } from 'react-hook-form';
+import { Meta } from '@storybook/react'
+import {
+  Box,
+  Flex,
+  FormLabel,
+  FormGuideline,
+  InputGroup,
+} from '@cap-collectif/ui'
+import FormControl from './FormControl'
+import { FieldInput } from './fieldInput'
+import { useForm } from 'react-hook-form'
 import * as React from 'react'
 
 const meta: Meta = {
-    title: 'Example',
-};
+  title: 'Example',
+  parameters: {
+    controls: { expanded: true },
+  },
+}
 
-export default meta;
+export default meta
 
 export const Default = () => {
-    const { control } = useForm({
-        mode: 'onChange',
-    });
-    const [value,setValue] = React.useState<string>('')
-    
+  const { control } = useForm({
+    mode: 'onChange',
+  })
+  const [value, setValue] = React.useState<string>('')
 
-    return (
-        <Box as="form" maxWidth="40%" margin="auto">
-            <FormControl name="firstName" control={control} isRequired>
-                <FormLabel label="Firstname" />
-                <FieldInput
-                    type="text"
-                    minLength={4}
-                    name="firstName"
-                    control={control}
-                    defaultValue="Patrick"
-                    placeholder="Jean"
-                    rules={{
-                        pattern: {
-                            value: /^[a-zA-Z]+$/,
-                            message: 'Uniquement des lettres',
-                        },
-                    }}
-                />
-            </FormControl>
+  return (
+    <Box as="form" maxWidth="40%" margin="auto">
+      <FormControl name="firstName" control={control} isRequired>
+        <FormLabel label="Firstname" />
+        <FieldInput
+          type="text"
+          minLength={4}
+          name="firstName"
+          control={control}
+          defaultValue="Patrick"
+          placeholder="Jean"
+          rules={{
+            pattern: {
+              value: /^[a-zA-Z]+$/,
+              message: 'Uniquement des lettres',
+            },
+          }}
+        />
+      </FormControl>
 
-            <FormControl name="fingers" control={control} isRequired>
-                <FormLabel label="Number of finger" />
-                <FieldInput
-                    type="number"
-                    name="fingers"
-                    control={control}
-                />
-            </FormControl>
+      <FormControl name="fingers" control={control} isRequired>
+        <FormLabel label="Number of finger" />
+        <FieldInput type="number" name="fingers" control={control} />
+      </FormControl>
 
-            <FormControl name="description" control={control} isRequired>
-                <FormLabel label="Description" />
-                <FormGuideline>Tell us about you</FormGuideline>
-                <FieldInput
-                    type="textarea"
-                    name="description"
-                    control={control}
-                    placeholder="Je suis heureux"
-                    rules={{
-                        pattern: {
-                            value: /^[a-zA-Z]+$/,
-                            message: 'Uniquement des lettres',
-                        },
-                    }}
-                />
-            </FormControl>
+      <FormControl name="description" control={control} isRequired>
+        <FormLabel label="Description" />
+        <FormGuideline>Tell us about you</FormGuideline>
+        <FieldInput
+          type="textarea"
+          name="description"
+          control={control}
+          placeholder="Je suis heureux"
+          rules={{
+            pattern: {
+              value: /^[a-zA-Z]+$/,
+              message: 'Uniquement des lettres',
+            },
+          }}
+        />
+      </FormControl>
 
-            <FormControl name="drink" control={control} isRequired>
-                <FormLabel label="Drink" />
-                <FormGuideline>Choose your drink(s)</FormGuideline>
-                <FieldInput
-                    type="checkbox"
-                    name="drink"
-                    id="drink"
-                    control={control}
-                    choices={[
-                        {
-                            id: 'coca',
-                            useIdAsValue: false,
-                            label: 'Coca cola',
-                        },
-                        {
-                            id: 'pepsi',
-                            useIdAsValue: false,
-                            label: 'Pepsi',
-                        },
-                    ]}
-                />
-            </FormControl>
+      <FormControl name="drink" control={control} isRequired>
+        <FormLabel label="Drink" />
+        <FormGuideline>Choose your drink(s)</FormGuideline>
+        <FieldInput
+          type="checkbox"
+          name="drink"
+          id="drink"
+          control={control}
+          choices={[
+            {
+              id: 'coca',
+              useIdAsValue: false,
+              label: 'Coca cola',
+            },
+            {
+              id: 'pepsi',
+              useIdAsValue: false,
+              label: 'Pepsi',
+            },
+          ]}
+        />
+      </FormControl>
 
-            <FormControl name="logo" control={control} isRequired>
-                <FormLabel label="Logo" />
-                <FormGuideline>Accepted type: jpg, png, svg.</FormGuideline>
-                <FieldInput
-                    type="uploader"
-                    name="logo"
-                    control={control}
-                    showThumbnail
-                    format="image/*"
-                />
-            </FormControl>
+      <FormControl name="logo" control={control} isRequired>
+        <FormLabel label="Logo" />
+        <FormGuideline>Accepted type: jpg, png, svg.</FormGuideline>
+        <FieldInput
+          type="uploader"
+          name="logo"
+          control={control}
+          showThumbnail
+          format="image/*"
+        />
+      </FormControl>
 
-            <FormControl name="holiday-pictures" control={control} isRequired>
-                <FormLabel label="Holiday Pictures" />
-                <FieldInput type="uploader" name="holiday-pictures" control={control} multiple />
-            </FormControl>
+      <FormControl name="holiday-pictures" control={control} isRequired>
+        <FormLabel label="Holiday Pictures" />
+        <FieldInput
+          type="uploader"
+          name="holiday-pictures"
+          control={control}
+          multiple
+        />
+      </FormControl>
 
-            <FormControl name="sober" control={control} isRequired>
-                <FieldInput type="checkbox" name="sober" control={control} id="sober-checkbox">
-                    Are you sober ?
-                </FieldInput>
-            </FormControl>
+      <FormControl name="sober" control={control} isRequired>
+        <FieldInput
+          type="checkbox"
+          name="sober"
+          control={control}
+          id="sober-checkbox"
+        >
+          Are you sober ?
+        </FieldInput>
+      </FormControl>
 
-            <FormControl name="happy" control={control} isRequired>
-                <Flex direction="row" align="center">
-                    <FieldInput type="switch" name="happy" control={control} id="happy-switch">
-                        Are you happy ?
-                    </FieldInput>
-                </Flex>
-            </FormControl>
+      <FormControl name="happy" control={control} isRequired>
+        <Flex direction="row" align="center">
+          <FieldInput
+            type="switch"
+            name="happy"
+            control={control}
+            id="happy-switch"
+          >
+            Are you happy ?
+          </FieldInput>
+        </Flex>
+      </FormControl>
 
-            <FormControl name="fastFood" control={control} isRequired>
-                <FormLabel label="Fast food" />
-                <FieldInput
-                    type="select"
-                    name="fastFood"
-                    control={control}
-                    placeholder="Choisir une option"
-                    options={[
-                        {
-                            value: 'mcdo',
-                            label: 'Mcdonald',
-                        },
-                        {
-                            value: 'bk',
-                            label: 'Burger king',
-                        },
-                    ]}
-                    clearable
-                />
-            </FormControl>
-          <FormControl name="codeInput" control={control} isRequired>
-            <FormLabel label="Code Input" />
-            <FieldInput
-              type="codeInput"
-              name="codeInput"
-              control={control}
-              length={6}
-              isVerified={value!==''}
-             onComplete={(code:string)=>{setValue(code)}}
-            />
-          </FormControl>
-          
-        </Box>
-    );
-};
+      <FormControl name="fastFood" control={control} isRequired>
+        <FormLabel label="Fast food" />
+        <FieldInput
+          type="select"
+          name="fastFood"
+          control={control}
+          placeholder="Choisir une option"
+          options={[
+            {
+              value: 'mcdo',
+              label: 'Mcdonald',
+            },
+            {
+              value: 'bk',
+              label: 'Burger king',
+            },
+          ]}
+          clearable
+        />
+      </FormControl>
+      <FormControl name="codeInput" control={control} isRequired>
+        <FormLabel label="Code Input" />
+        <FieldInput
+          type="codeInput"
+          name="codeInput"
+          control={control}
+          length={6}
+          isVerified={value !== ''}
+          onComplete={(code: string) => {
+            setValue(code)
+          }}
+        />
+      </FormControl>
+      <InputGroup>
+        <FormLabel label="Choisissez une couleur :" />
+        <FormControl name="CountryCode" control={control} isRequired>
+          <FieldInput
+            type="flagSelect"
+            name="CountryCode"
+            control={control}
+            placeholder="Choisir un pays"
+          />
+        </FormControl>
+        <FormControl name="phoneNumber" control={control} isRequired>
+          <FieldInput
+            type="tel"
+            name="phoneNumber"
+            maxLength={10}
+            control={control}
+          />
+        </FormControl>
+      </InputGroup>
+    </Box>
+  )
+}
--- src/components/fieldInput/FieldInput.tsx
+++ src/components/fieldInput/FieldInput.tsx
@@ -5,73 +5,87 @@
  *  But TS seems to not understand.
  */
 
-import type { FC } from 'react';
-import type { BaseField, AllFieldTypes } from './FieldInput.type';
-import { useController } from 'react-hook-form';
-import { Input, TextArea, Switch, Checkbox, InputNumber,CodeInput } from '@cap-collectif/ui';
-import MultipleCheckbox from '../MultipleCheckbox';
-import Select from '../Select';
-import Uploader from '../uploader/Uploader';
-import { useIntl } from 'react-intl';
-import { getEmailRule, getMinLengthRule } from './FieldInput.utils';
+import type { FC } from 'react'
+import type { BaseField, AllFieldTypes } from './FieldInput.type'
+import { useController } from 'react-hook-form'
+import {
+  Input,
+  TextArea,
+  Switch,
+  Checkbox,
+  InputNumber,
+  CodeInput,
+} from '@cap-collectif/ui'
+import MultipleCheckbox from '../MultipleCheckbox'
+import Select from '../Select'
+import Uploader from '../uploader/Uploader'
+import { useIntl } from 'react-intl'
+import { getEmailRule, getMinLengthRule } from './FieldInput.utils'
+import { FlagSelect } from '../FlagSelect'
 
-export type FieldInputProps = BaseField & AllFieldTypes;
+export type FieldInputProps = BaseField & AllFieldTypes
 
 export const FieldInput: FC<FieldInputProps> = ({
-    name,
-    type = 'text',
-    control,
-    rules,
-    defaultValue,
-    onChange,
-    ...props
+  name,
+  type = 'text',
+  control,
+  rules,
+  defaultValue,
+  onChange,
+  ...props
 }) => {
-    const intl = useIntl();
-    const minLengthRule = props.minLength ? getMinLengthRule(props.minLength, intl) : undefined;
-    const emailRule = props.type === 'email' ? getEmailRule(intl) : undefined;
+  const intl = useIntl()
+  const minLengthRule = props.minLength
+    ? getMinLengthRule(props.minLength, intl)
+    : undefined
+  const emailRule = props.type === 'email' ? getEmailRule(intl) : undefined
 
-    const { field } = useController({
-        name,
-        control,
-        rules: {
-            minLength: minLengthRule,
-            pattern: emailRule,
-            ...rules,
-        },
-        defaultValue,
-    });
+  const { field } = useController({
+    name,
+    control,
+    rules: {
+      minLength: minLengthRule,
+      pattern: emailRule,
+      ...rules,
+    },
+    defaultValue,
+  })
 
-    const handleOnChange = (e): void => {
-        if(onChange) onChange(e);
-        field.onChange(e);
-    }
-    
+  const handleOnChange = (e): void => {
+    if (onChange) onChange(e)
+    field.onChange(e)
+  }
 
-    switch (type) {
-        default:
-        case 'text':
-        case 'email':
-        case 'password':
-            return <Input type={type} {...props} {...field} onChange={handleOnChange} />;
-        case 'number':
-            return <InputNumber {...props} {...field} onChange={handleOnChange} />;
-        case 'textarea':
-            return <TextArea {...props} {...field} onChange={handleOnChange} />;
-        case 'select':
-            return <Select {...props} {...field} onChange={handleOnChange} />;
-        case 'switch':
-            return <Switch {...props} {...field} onChange={handleOnChange} />;
-        case 'checkbox':
-            return props?.choices ? (
-                <MultipleCheckbox {...props} {...field} onChange={handleOnChange} />
-            ) : (
-                <Checkbox {...props} {...field} onChange={handleOnChange} />
-            );
-        case 'uploader':
-            return <Uploader {...props} {...field} />;
-        case 'codeInput':
-            return <CodeInput {...props} {...field} />;
-    }
-};
+  switch (type) {
+    default:
+    case 'text':
+    case 'email':
+    case 'password':
+    case 'tel':
+      return (
+        <Input type={type} {...props} {...field} onChange={handleOnChange} />
+      )
+    case 'number':
+      return <InputNumber {...props} {...field} onChange={handleOnChange} />
+    case 'textarea':
+      return <TextArea {...props} {...field} onChange={handleOnChange} />
+    case 'select':
+      return <Select {...props} {...field} onChange={handleOnChange} />
+    case 'switch':
+      return <Switch {...props} {...field} onChange={handleOnChange} />
+    case 'checkbox':
+      return props?.choices ? (
+        <MultipleCheckbox {...props} {...field} onChange={handleOnChange} />
+      ) : (
+        <Checkbox {...props} {...field} onChange={handleOnChange} />
+      )
+    case 'uploader':
+      return <Uploader {...props} {...field} />
+    case 'codeInput':
+      return <CodeInput {...props} {...field} />
+    case 'flagSelect':
+      return <FlagSelect {...props} {...field} onChange={handleOnChange} />
+  }
+}
 
-export default FieldInput;
+export default FieldInput
