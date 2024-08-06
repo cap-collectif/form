ab515ab05cdb582d9db1b1a7a7076278d4ffbbf9
myracodes <myriam@cap-collectif.com>
2024-08-06 18:45:46 +0200

    feat: add forwardRef + id and placeholder props

--- src/components/dateHour/DateHour.tsx
+++ src/components/dateHour/DateHour.tsx
@@ -9,90 +9,105 @@ import {
   DateInputProps,
 } from '@cap-collectif/ui'
 import moment from 'moment'
-import { FC, useEffect, useState } from 'react'
+import { forwardRef, Ref, useEffect, useState } from 'react'
 
 export interface DateHourProps extends Omit<BoxPropsOf<'input'>, 'onChange'> {
   readonly isDisabled?: boolean
   readonly isInvalid?: boolean
   readonly variantSize?: CapInputSize
   readonly dateInputProps?: Partial<DateInputProps>
+  readonly value?: string
+  readonly id?: string
+  readonly placeholder?: string
+  readonly ref?: Ref<HTMLInputElement | null>
 }
 
 const DATE_FORMAT = 'YYYY-MM-DD H:mm:ss'
 
-export const DateHour: FC<DateHourProps> = ({
-  onChange,
-  value,
-  variantSize = CapInputSize.Sm,
-  isDisabled,
-  isInvalid,
-  dateInputProps = {},
-}) => {
-  const { colors } = useTheme()
-  const initialDate = value ? moment(value, DATE_FORMAT) : null
+export const DateHour = forwardRef<HTMLInputElement, DateHourProps>(
+  (
+    {
+      onChange,
+      value,
+      id,
+      placeholder,
+      variantSize = CapInputSize.Sm,
+      isDisabled,
+      isInvalid,
+      dateInputProps = {},
+    },
+    ref,
+  ) => {
+    const { colors } = useTheme()
+    const initialDate = value ? moment(value, DATE_FORMAT) : null
 
-  const [hourValue, setHourValue] = useState(
-    initialDate ? initialDate.format('hh:mm') : null,
-  )
-  const [dateValue, setDateValue] = useState<moment.Moment | null>(initialDate)
+    const [hourValue, setHourValue] = useState(
+      initialDate ? initialDate.format('hh:mm') : null,
+    )
+    const [dateValue, setDateValue] =
+      useState<moment.Moment | null>(initialDate)
 
-  useEffect(() => {
-    if (!dateValue) {
-      onChange(null)
-      return
-    }
-    const formattedHour = moment(hourValue ?? '00:00', 'hh:mm')
-    const dateHour = moment(dateValue ?? moment.now())
-      .set({
-        hour: formattedHour.get('hour'),
-        minute: formattedHour.get('minute'),
-      })
-      .format(DATE_FORMAT)
-    onChange(dateHour)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hourValue, dateValue])
+    useEffect(() => {
+      if (!dateValue) {
+        onChange(null)
+        return
+      }
+      const formattedHour = moment(hourValue ?? '00:00', 'hh:mm')
+      const dateHour = moment(dateValue ?? moment.now())
+        .set({
+          hour: formattedHour.get('hour'),
+          minute: formattedHour.get('minute'),
+        })
+        .format(DATE_FORMAT)
+      onChange(dateHour)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [hourValue, dateValue])
 
-  return (
-    <Box
-      sx={{
-        '& .cap-date-input': {
-          borderRight: '0px !important',
-          borderTopRightRadius: '0px !important',
-          borderBottomRightRadius: '0px !important',
-        },
-        '& .cap-date-input + div': { flex: 'none' },
-        '& .cap-hour-input > div': { borderLeft: '0px !important' },
-        '&:focus-within *':
-          !isDisabled && !isInvalid
-            ? { borderColor: `${colors.blue[500]} !important` }
-            : {},
-      }}
-    >
-      <InputGroup sx={{ flexWrap: 'nowrap !important' }}>
-        <DateInput
-          onChange={(newDateValue: React.ChangeEvent<HTMLInputElement>) => {
-            const value = newDateValue.target.value
-              ? moment(newDateValue.target.value, DATE_FORMAT)
-              : null
-            setDateValue(value)
-          }}
-          value={dateValue?.format('YYYY-MM-DD')}
-          variantSize={variantSize}
-          isDisabled={isDisabled}
-          isInvalid={isInvalid}
-          {...dateInputProps}
-        />
-        <HourInput
-          onChange={(newHourValue: string) => {
-            setHourValue(newHourValue)
-          }}
-          variantSize={variantSize}
-          isDisabled={isDisabled}
-          isInvalid={isInvalid}
-        />
-      </InputGroup>
-    </Box>
-  )
-}
+    return (
+      <Box
+        sx={{
+          '& .cap-date-input': {
+            borderRight: '0px !important',
+            borderTopRightRadius: '0px !important',
+            borderBottomRightRadius: '0px !important',
+          },
+          '& .cap-date-input + div': { flex: 'none' },
+          '& .cap-hour-input > div': { borderLeft: '0px !important' },
+          '&:focus-within *':
+            !isDisabled && !isInvalid
+              ? { borderColor: `${colors.blue[500]} !important` }
+              : {},
+        }}
+      >
+        <InputGroup sx={{ flexWrap: 'nowrap !important' }}>
+          <DateInput
+            onChange={(newDateValue: React.ChangeEvent<HTMLInputElement>) => {
+              const value = newDateValue.target.value
+                ? moment(newDateValue.target.value, DATE_FORMAT)
+                : null
+              setDateValue(value)
+            }}
+            value={dateValue?.format('YYYY-MM-DD')}
+            variantSize={variantSize}
+            isDisabled={isDisabled}
+            isInvalid={isInvalid}
+            {...dateInputProps}
+            ref={ref}
+          />
+          <HourInput
+            // value={hourValue ?? placeholder ?? null}
+            onChange={(newHourValue: string) => {
+              setHourValue(newHourValue)
+            }}
+            variantSize={variantSize}
+            isDisabled={isDisabled}
+            isInvalid={isInvalid}
+            id={id}
+          />
+        </InputGroup>
+      </Box>
+    )
+  },
+)
 
 export default DateHour
